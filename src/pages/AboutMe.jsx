1287f14251f2f2bbe0db1d781fe596b7dd1d6f90
KrisMoe <raini.night161@gmail.com>
2024-08-09 17:27:34 -0500

    added web pages

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,8 +10,16 @@ export default function AboutMe() {
   
   return (
 
-      <Container className={Layout.center+" "+ Layout.padcontainer} >
-        <Row><Col>About Me</Col></Row>
+      <Container className={" "+ Layout.padcontainer} >
+        <Row><Col><h1>About Me</h1></Col></Row>
+        <Row><Col>I’ve been interested in computer science since high school. 
+        I was initially drawn to it because I love mathematical applications and creating powerful art.
+         Since starting my degree I’ve also found that I like creating websites and video games. 
+         I taught myself two different game engines and have created multiple websites.
+          One thing that I have especially enjoyed about learning computer science is how to make my code more efficient. 
+          When I work on a project I look at the whole system and figure out what the potential for improvement is. 
+          This mindset has helped me save time and avoid bugs on numerous projects. 
+        </Col></Row>
       </Container>
   
   )
